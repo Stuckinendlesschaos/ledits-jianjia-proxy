4ffb6c29c799262780dc9d250de45f7f088b0e7c
hyperbloger <1578551557@qq.com>
2023-08-07 02:43:55 +0800

    rewrite sega_val issues to match frontend logic

--- client.py
+++ client.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask import request
+from flask import send_file, jsonify
+
+
 from  flask_cors import CORS
 from gradio_client import Client
 
 from constants import *
-import base64
+import io,base64
 
 class Entity():
     Imagepath: str = ""
@@ -39,7 +43,7 @@ def ledits():
         # 如果ImageFile上传文件不符合规范
         if request.files.get('ImageFile') == None:
             return jsonify({'key':"error"})     
-            
+
         # 保证文件是有效的!!!
         file_obj = request.files['ImageFile']
         if file_obj != None:
@@ -49,10 +53,10 @@ def ledits():
 		        api_name="/reset_do_inversion"
             )
             
-            Entity.Imagepath = client.predict(
-                Entity.Imagepath,
-                api_name="/crop_image"
-            )
+            # Entity.Imagepath = client.predict(
+            #     Entity.Imagepath,
+            #     api_name="/crop_image"
+            # )
             
             tar_prompt = client.predict(
                 Entity.Imagepath,
@@ -60,9 +64,9 @@ def ledits():
             )
             
              
-            if request.form.get('Seed_switch') != None:
+            if request.form.get('Seed_switch') != None: 
                 # 种子开关是False, 直接DDPM Inversion,不设置程序摇号
-                if request.form.get('Seed_switch') == 'True':
+                if request.form.get('Seed_switch') == 'True' or request.form.get('Seed_switch') == 'true':
                     Entity.random_enabled = True
                     # 撒个种子
                     seed = client.predict(
@@ -84,21 +88,19 @@ def ledits():
                 DEFAULT_TARGET_GUIDANCE_SCALE,	# int | float (numeric value between 1 and 30) in 'Guidance Scale' Slider component
                 api_name="/DDPM_load_and_invert"
             )
-            
-                
+               
             # SEGAS Workplace
-            if request.form.getlist('sega_val0') != []:  
-                allocated_val0 = request.form.getlist('sega_val0')
+            if request.form.get('sega_val0') != "":  
+                allocated_val0 = request.form.get('sega_val0')
                 # 预处理
-                allocated_val0 = allocated_val0[0].strip('[').strip(']')  
+                # allocated_val0 = allocated_val0[0].strip('[').strip(']')  
                 # list的第一个参数是type,第二个参数是prompt,第三个参数是removeButton?
-            
                 allocated_val0 = allocated_val0.split(',')
-                # prompt不为空
-                if allocated_val0[1][1:-1]!= '':
-                    Entity.sega_val0 = allocated_val0[0][1:-1]
-                    Entity.edit_concept_0 = allocated_val0[1][1:-1]
-                    if allocated_val0[2] == 'True':
+                # 默认长度为3 且 prompt不为空
+                if len(allocated_val0) > 2 and allocated_val0[1] != '':
+                    Entity.sega_val0 = allocated_val0[0]
+                    Entity.edit_concept_0 = allocated_val0[1]
+                    if allocated_val0[2] == 'True' or allocated_val0[2] == 'true':
                         Entity.neg_guidance_0 = True
                     allocated_para0 = client.predict(
                         Entity.sega_val0,
@@ -106,17 +108,16 @@ def ledits():
                     # 分配参数
                     Entity.guidnace_scale_0,Entity.warmup_0,Entity.threshold_0 = allocated_para0[0],allocated_para0[1],allocated_para0[2]
                 
-            if request.form.getlist('sega_val1') != []:  
-                allocated_val1 = request.form.getlist('sega_val1')
+            if request.form.getlist('sega_val1') != "":  
+                allocated_val1 = request.form.get('sega_val1')
                 # 预处理
-                allocated_val1 = allocated_val1[0].strip('[').strip(']')  
                 # list的第一个参数是type,第二个参数是negativePrompt,第三个参数是remove Concept?
                 allocated_val1 = allocated_val1.split(',')
                 # negativePrompt不为空
-                if allocated_val1[1][1:-1] != '':
-                    Entity.sega_val1 = allocated_val1[0][1:-1]
-                    Entity.edit_concept_1 = allocated_val1[1][1:-1]
-                    if allocated_val1[2] == 'True':
+                if len(allocated_val1) > 2 and allocated_val1[1] != '':
+                    Entity.sega_val1 = allocated_val1[0]
+                    Entity.edit_concept_1 = allocated_val1[1]
+                    if allocated_val1[2] == 'True' or allocated_val1[2] == 'true':
                         Entity.neg_guidance_1 = True
                     allocated_para1 = client.predict(
                         Entity.sega_val1,
@@ -153,18 +154,21 @@ def ledits():
                 DEFAULT_SOURCE_GUIDANCE_SCALE,	# int | float  in 'Source Guidance Scale' Number component
                 api_name="/LEDITS_edit"
             )
-        
+            
+            
+        # base64格式
         with open(resVal[0],"rb") as img_file:
-            Response = base64.b64encode(img_file.read())
+          Response = base64.b64encode(img_file.read())
+          return Response
         
-        
-        
-#     # 获取前端交互的数值
-#     # data = request.get_data()
-    
-    return jsonify({'key': Response})
-    
-    
+        # with open(resVal[0],"rb") as img_file:
+        #     return send_file(
+        #         # 转二进制流
+        #         io.BytesIO(img_file.read()),
+        #         download_name = file_obj.filename,
+        #         # mimetype = "image/png"
+        #     )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
